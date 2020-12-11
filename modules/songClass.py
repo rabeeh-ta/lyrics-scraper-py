4ddede2db5d0b8fc8a9d3f169621ad10989e5ec1
rabeeh-ta <rabeehchemnad@gmail.com>
2020-12-11 23:15:45 +0530

    New attribute for to check if there is lyrics presaved

--- modules/songClass.py
+++ modules/songClass.py
@@ -2,8 +2,8 @@ import bs4
 
 
 class Song:
-    def __init__(self, name, title, artist):
-        self.name = name
+    def __init__(self, title, artist, location, lyricsFound=False):
         self.title = title
         self.artist = artist
-
+        self.location = location
+        self.lyricsFound = lyricsFound
